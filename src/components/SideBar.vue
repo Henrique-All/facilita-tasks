bd35e30b7f401aae095d15d1604a8267f9a6e254
froztiin <carlinhos.biocraft@gmail.com>
2024-02-07 00:00:12 -0300

    first commit

---
+++ src/components/SideBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="logo">
+    <img src="./icons/IconEcosystem.vue" alt="" srcset="" />
+  </div>
+  <div>
+    <nav>
+      <a href="#"><fa icon="chart-column" />Dashboard</a>
+      <a href="#"
+        ><span><fa icon="circle-check" /></span>Tarefas</a
+      >
+      <a href="#"
+        ><span><fa icon="cogs" /></span>Configurações</a
+      >
+    </nav>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+library.add(fas, far)
+
+createApp(App).component('fa', FontAwesomeIcon).mount('#app')
