aba0b11904ca199f45188283d5038bd001030a7f
CH <carlinhos.biocraft@gmail.com>
2024-02-07 00:28:29 -0300

    feat: criação da sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,20 +1,61 @@
 <template>
-  <div class="logo">
-    <img src="./icons/IconEcosystem.vue" alt="" srcset="" />
-  </div>
-  <div>
-    <nav>
-      <a href="#"><fa icon="chart-column" />Dashboard</a>
+  <main class="main-side">
+    <!-- logo -->
+    <div class="logo">
+      <img src="./icons/logo.svg" alt="logo" />
+    </div>
+    <!-- menu de navegação -->
+    <nav class="main-nav">
+      <a href="#"><fa icon="chart-column" style="font-size: 35px" />Dashboard</a>
       <a href="#"
-        ><span><fa icon="circle-check" /></span>Tarefas</a
+        ><span><fa icon="circle-check" style="font-size: 35px" /></span>Tarefas</a
       >
       <a href="#"
-        ><span><fa icon="cogs" /></span>Configurações</a
+        ><span><fa icon="cogs" style="font-size: 35px" /></span>Configurações</a
       >
     </nav>
-  </div>
+  </main>
 </template>
+
+<!-- funcional -->
 <script>
 export default {}
 </script>
-<style scoped></style>
+
+<!-- styles -->
+<style scoped>
+/* Editando primeiro o main */
+.main-side {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 9%;
+  background-color: #0a79ff;
+  justify-content: center;
+  align-items: center;
+}
+
+/* Propriedade do logo */
+.logo {
+  position: absolute;
+  top: 10px;
+}
+
+/* Configuração da navegação */
+.main-nav {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  a {
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    align-items: center;
+    font-size: 15px;
+    font-weight: 700;
+    gap: 10px;
+  }
+}
+</style>
